7befa712d54f70646d22f99cb1b44e3233e0c230
Arthur Luiz <artluiz.05@gmail.com>
2025-03-24 22:55:07 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -5,8 +5,7 @@ import {
   createUserWithEmailAndPassword,
   onAuthStateChanged,
   setPersistence,
-  browserSessionPersistence,
-
+  browserSessionPersistence
 } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
 import { 
   getFirestore,
@@ -22,22 +21,23 @@ import {
   addDoc,
   limit,
   Timestamp
-} from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
+} from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js"; // Correção importante aqui
 
 const firebaseConfig = {
   apiKey: "AIzaSyBSDlff_k_i0_4O8J2UIXS67kDY2H5pFuI",
   authDomain: "paperless-kc.firebaseapp.com",
   projectId: "paperless-kc",
-  storageBucket: "paperless-kc.firebasestorage.app",
   messagingSenderId: "11444788144",
-  appId: "1:11444788144:web:454a9c37faec71d5d69475",
+  appId: "1:11444788144:web:454a9c37faec71d5d69475"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
-setPersistence(auth, browserSessionPersistence).then(() => console.log("Persistência configurada com sucesso!"))
-.catch((error) => console.error("Erro na persistência:", error));
+
+setPersistence(auth, browserSessionPersistence)
+  .then(() => console.log("Persistência configurada!"))
+  .catch((error) => console.error("Erro na persistência:", error));
 
 export { 
   auth,
@@ -56,5 +56,5 @@ export {
   deleteDoc,
   addDoc,
   limit,
-  Timestamp,
+  Timestamp
 };
